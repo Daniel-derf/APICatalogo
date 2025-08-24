e3af38489d39d37c4a98ed24ae793339c6f045cf
daniel-derf <danielpardinho321@gmail.com>
2025-08-23 22:01:04 -0300

    feat: data annotations

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
 
+
+[Table("Categorias")]
 public class Categoria
 {
     public Categoria()
     {
         Produtos = new Collection<Produto>();
     }
+
+    [Key]
     public int CategoriaId { get; set; }
+
+    [Required(ErrorMessage = "O nome da categoria é obrigatório")]
+    [StringLength(100, ErrorMessage = "O nome da categoria deve ter no máximo 100 caracteres")]
     public string? Nome { get; set; }
 
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
-
     public ICollection<Produto>? Produtos { get; set; }
 }
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,15 +1,37 @@
-﻿namespace APICatalogo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace APICatalogo.Models;
+
+[Table("Produtos")]
 public class Produto
 {
+    [Key]
     public int ProdutoId { get; set; }
+
+    [Required(ErrorMessage = "O nome do produto é obrigatório")]
+    [StringLength(100, ErrorMessage = "O nome do produto deve ter no máximo 100 caracteres")]
     public string? Nome { get; set; }
+
+    [Required(ErrorMessage = "A descrição do produto é obrigatória")]
+    [StringLength(300, ErrorMessage = "A descrição do produto deve ter no máximo 300 caracteres")]
     public int Descricao { get; set; }
+
+    [Required(ErrorMessage = "O preço do produto é obrigatório")]
+    [Column(TypeName = "decimal(10,2)")]
     public decimal Preco { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
+
+    [Required(ErrorMessage = "A quantidade em estoque do produto é obrigatória")]
     public float Estoque { get; set; }
+
+    [Required]
     public DateTime DataCadastro { get; set; }
 
+    [ForeignKey("Categoria")]
     public int CategoriaId { get; set; }
     public Categoria? Categoria { get; set; }
-    }
+}
